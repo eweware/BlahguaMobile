77f9cfe62cab834553709c2ab26738bcea585788
Dave Vromay <davevr@eweware.com>
2014-01-06 08:26:07 -0800

    initial checkin

---
+++ BlahguaCore/HelperClasses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinPhoneBlahgua
+{
+
+    public class SigninStatus
+    {
+        public int M { get; set; }
+        public string loggedIn { get; set; }
+        public int S { get; set; }
+    }
+
+    public class RecoveryInfo
+    {
+        public string E { get; set; }
+    }
+}
