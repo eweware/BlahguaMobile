5008da7718f77074161c5d4a9cf79c0b9950dafc
Pavel Sipaylo <pavel.sipaylo@gmail.com>
2014-06-05 15:00:30 +0300

    Completed profile edit view

---
+++ BlahguaIOS/Helpers/UIImageHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+
+namespace BlahguaMobile.IOS
+{
+	public static class UIImageHelper
+	{
+		public static UIImage ImageFromUrl(string uri)
+		{
+			using(var url = new NSUrl(uri))
+			{
+				using(var data = NSData.FromUrl(url))
+				{
+					return UIImage.LoadFromData(data);
+				}
+			}
+		}
+	}
+}
+
