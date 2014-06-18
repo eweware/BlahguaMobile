5d87a001cef1bc82615e66a2cb94742eff6d38c3
Dave Vronay <davevr@eweware.com>
2014-06-18 09:43:50 -0700

    more image updates

--- BlahguaIOS/Constants/BGAppearanceConstants.cs
+++ BlahguaIOS/Constants/BGAppearanceConstants.cs
@@ -17,6 +17,8 @@ namespace BlahguaMobile.IOS
 		public static readonly RectangleF OpenedRightViewContainerFrame = new RectangleF(0, 0, 320, UIScreen.MainScreen.Bounds.Height);
 
 		public static readonly RectangleF RightViewFrame = new RectangleF(163, 64, 157, UIScreen.MainScreen.Bounds.Height - 64);
+        public static readonly UIColor DarkBrown = UIColor.FromRGB(63, 43, 47);
+        public static readonly UIColor TealGreen = UIColor.FromRGB(96, 191, 164);
 	}
 }
 
