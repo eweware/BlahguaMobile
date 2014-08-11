be45454bb5265a4313e31a899da7a3930fd9dcb1
Dave Vronay <davevr@eweware.com>
2014-08-11 06:50:13 -0700

    moved image upload fix global

--- BlahguaIOS/Helpers/UIImageHelper.cs
+++ BlahguaIOS/Helpers/UIImageHelper.cs
@@ -2,6 +2,8 @@
 
 using MonoTouch.UIKit;
 using MonoTouch.Foundation;
+using MonoTouch.CoreGraphics;
+using System.Drawing;
 
 namespace BlahguaMobile.IOS
 {
@@ -17,6 +19,116 @@ namespace BlahguaMobile.IOS
 				}
 			}
 		}
+
+        public static UIImage ScaleAndRotateImage(UIImage image)
+        {
+            int kMaxResolution = 1024; // Or whatever
+
+            CGImage imgRef = image.CGImage;
+            float width = imgRef.Width;
+            float height = imgRef.Height;
+            CGAffineTransform transform = CGAffineTransform.MakeIdentity();
+            RectangleF  bounds = new RectangleF(0,0,width, height);
+
+            if (width > kMaxResolution || height > kMaxResolution)
+            {
+                float ratio = width / height;
+
+                if (ratio > 1)
+                {
+                    bounds.Width = kMaxResolution;
+                    bounds.Height = bounds.Width / ratio;
+                }
+                else
+                {
+                    bounds.Height = kMaxResolution;
+                    bounds.Width = bounds.Height * ratio;
+                }
+            }
+
+            float scaleRatio = bounds.Width / width;
+            SizeF imageSize = new SizeF(width, height);
+            float boundHeight;
+            UIImageOrientation orient = image.Orientation;
+
+            switch (orient)
+            {
+                case UIImageOrientation.Up: //EXIF = 1
+                    transform = CGAffineTransform.MakeIdentity();
+                    break;
+
+                case UIImageOrientation.UpMirrored: //EXIF = 2
+                    transform = CGAffineTransform.MakeTranslation(imageSize.Width, 0.0f);
+                    transform = CGAffineTransform.Scale(transform, -1.0f, 1.0f);
+                    break;
+
+                case UIImageOrientation.Down: //EXIF = 3
+                    transform = CGAffineTransform.MakeTranslation(imageSize.Width, imageSize.Height);
+                    transform = CGAffineTransform.Rotate(transform, (float)Math.PI);
+                    break;
+
+                case UIImageOrientation.DownMirrored: //EXIF = 4
+                    transform = CGAffineTransform.MakeTranslation(0.0f, imageSize.Height);
+                    transform = CGAffineTransform.Scale(transform, 1.0f, -1.0f);
+                    break;
+
+                case UIImageOrientation.LeftMirrored: //EXIF = 5
+                    boundHeight = bounds.Height;
+                    bounds.Height = bounds.Width;
+                    bounds.Width = boundHeight;
+                    transform = CGAffineTransform.MakeTranslation(imageSize.Height, imageSize.Width);
+                    transform = CGAffineTransform.Scale(transform, -1.0f, 1.0f);
+                    transform = CGAffineTransform.Rotate(transform, 3.0f * (float)Math.PI / 2.0f);
+                    break;
+
+                case UIImageOrientation.Left: //EXIF = 6
+                    boundHeight = bounds.Height;
+                    bounds.Height = bounds.Width;
+                    bounds.Width = boundHeight;
+                    transform = CGAffineTransform.MakeTranslation(0.0f, imageSize.Width);
+                    transform = CGAffineTransform.Rotate(transform, 3.0f * (float)Math.PI / 2.0f);
+                    break;
+
+                case UIImageOrientation.RightMirrored: //EXIF = 7
+                    boundHeight = bounds.Height;
+                    bounds.Height = bounds.Width;
+                    bounds.Width = boundHeight;
+                    transform = CGAffineTransform.MakeScale(-1.0f, 1.0f);
+                    transform = CGAffineTransform.Rotate(transform, (float)Math.PI / 2.0f);
+                    break;
+
+                case UIImageOrientation.Right: //EXIF = 8
+                    boundHeight = bounds.Height;
+                    bounds.Height = bounds.Width;
+                    bounds.Width = boundHeight;
+                    transform = CGAffineTransform.MakeTranslation(imageSize.Height, 0.0f);
+                    transform = CGAffineTransform.Rotate(transform, (float)Math.PI / 2.0f);
+                    break;
+            }
+
+            UIGraphics.BeginImageContext (bounds.Size);
+            CGContext context = UIGraphics.GetCurrentContext ();
+
+            if (orient == UIImageOrientation.Right || orient == UIImageOrientation.Left) 
+            {
+                context.ScaleCTM(-scaleRatio, scaleRatio);
+                context.TranslateCTM (-height, 0.0f);
+            }
+            else 
+            {
+                context.ScaleCTM (scaleRatio, -scaleRatio);
+                context.TranslateCTM (0.0f, -height);
+            }
+
+            context.ConcatCTM (transform);
+
+            context.DrawImage (new RectangleF(0,0, width, height), imgRef);
+            UIImage imageCopy = UIGraphics.GetImageFromCurrentImageContext ();
+            UIGraphics.EndImageContext ();
+
+            return imageCopy;
+
+        }
 	}
 }
 
