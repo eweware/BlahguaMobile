8725eb2236987058dd6ac80c0bf6e4ba7989dc28
Dave Vronay <davevr@eweware.com>
2014-08-12 01:02:29 -0700

    cleanup of unused files

--- BlahguaIOS/View Controllers/BGHistoryDetailViewController.designer.cs
+++ BlahguaIOS/View Controllers/BGHistoryDetailViewController.designer.cs
@@ -15,9 +15,6 @@ namespace BlahguaMobile.IOS
 	partial class BGHistoryDetailViewController
 	{
 		[Outlet]
-		MonoTouch.UIKit.UITableView detailMainTable { get; set; }
-
-		[Outlet]
 		[GeneratedCode ("iOS Designer", "1.0")]
 		UITableView view { get; set; }
 
